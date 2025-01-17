bdc1495add186118ddf41ac831fad953b1cf949a
mpragmas <mpragmas@gmail.com>
2025-01-17 21:38:19 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,28 +26,42 @@ const cardDetails = [
 function App() {
   return (
     <>
-      <div className="relative mb-36 rounded-b-[200px] bg-blue-500 px-40 pb-60 pt-12 text-white">
+      <div className="relative mb-36 rounded-b-[200px] bg-[url('../public/banner-bg.jpeg')] px-40 pb-60 pt-12 text-white">
         <nav className="flex items-center justify-between text-white">
           <div className="font-bold">
             <img src="../public/logo.png" />
           </div>
 
-          <div className="flex items-center gap-10 text-sm font-light">
-            <p className="rounded-3xl bg-blue-400 px-6 py-2">Home</p>
-            <p>Our Shop</p>
-            <p>Product Details</p>
-            <p>Contact Us </p>
-            <p className="rounded-3xl bg-red-400 px-6 py-2">SIGN IN</p>
+          <div className="flex items-center gap-5 text-sm font-light">
+            <p className="cursor-pointer rounded-3xl bg-blue-400 px-6 py-2">
+              Home
+            </p>
+            <p className="cursor-pointer rounded-3xl px-6 py-2 hover:bg-blue-400">
+              Our Shop
+            </p>
+            <p className="cursor-pointer rounded-3xl px-6 py-2 hover:bg-blue-400">
+              Product Details
+            </p>
+            <p className="cursor-pointer rounded-3xl px-6 py-2 hover:bg-blue-400">
+              Contact Us
+            </p>
+            <p className="cursor-pointer rounded-3xl bg-red-400 px-6 py-2">
+              SIGN IN
+            </p>
           </div>
         </nav>
 
         <section className="mt-36">
-          <div className="grid-cols-mycustom grid gap-40">
-            <div className="mt-12">
+          <div className="grid grid-cols-mycustom gap-40">
+            <div className="relative mt-12">
               <p>WELCOME TO LUGX</p>
               <p className="mt-6 text-5xl font-extrabold">
                 BEST GAMING SITE EVER
               </p>
+              <img
+                src="../public/caption-dec.png"
+                className="absolute left-60 -translate-x-1/2"
+              />
               <p className="mt-12 text-sm font-light">
                 LUGX Gaming is free Bootstrap 5 HTML CSS website template for
                 your gaming websites. You can download and use this layout for
@@ -58,14 +72,13 @@ function App() {
                   placeholder="Type Something"
                   className="h-11 w-[400px] rounded-3xl bg-white px-6 py-6 font-light placeholder:text-gray-400"
                 />
-                <button className="absolute right-48 h-12 rounded-3xl bg-red-400 px-6">
-                  SIGN IN
+                <button className="absolute right-48 h-12 rounded-3xl bg-red-400 px-6 hover:bg-blue-500">
+                  SEARCH NOW
                 </button>
               </div>
             </div>
             <div className="relative">
               <img src="../public/banner-image.jpg" className="rounded-3xl" />
-
               <p className="absolute bottom-0 left-0 inline-block -translate-x-1/2 translate-y-1/2 rounded-full bg-red-400 px-2 py-6 text-2xl font-bold">
                 -40%
               </p>
@@ -77,7 +90,7 @@ function App() {
         </section>
         <div className="absolute bottom-0 z-50 translate-y-1/2 space-x-6">
           {cardDetails.map((el) => (
-            <Card icon={el.icon} text={el.text} />
+            <Card icon={el.icon} text={el.text} key={el.text} />
           ))}
         </div>
       </div>
